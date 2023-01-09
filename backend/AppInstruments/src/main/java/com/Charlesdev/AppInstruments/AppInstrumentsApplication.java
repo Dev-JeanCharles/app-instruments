a4f765a7941e29cf25e5e251610af7dca374573c
JeanCharles14 <jeancduasan@gmail.com>
2023-01-09 19:55:59 -0300

    change user

--- backend/AppInstruments/src/main/java/com/Charlesdev/AppInstruments/AppInstrumentsApplication.java
+++ backend/AppInstruments/src/main/java/com/Charlesdev/AppInstruments/AppInstrumentsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppInstrumentsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AppInstrumentsApplication.class, args);
 	}
