8aa3e2d108e50b79c280615b32edce739dd88675
Dev-JeanSantos <jeancbsan@gmail.com>
2023-01-09 19:53:47 -0300

    created project

---
+++ backend/AppInstruments/src/main/java/com/Charlesdev/AppInstruments/AppInstrumentsApplication.java
@@ -0,0 +1,13 @@
+package com.Charlesdev.AppInstruments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInstrumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInstrumentsApplication.class, args);
+	}
+
+}
---
+++ backend/AppInstruments/src/test/java/com/Charlesdev/AppInstruments/AppInstrumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Charlesdev.AppInstruments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInstrumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
